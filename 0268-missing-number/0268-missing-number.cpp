f25ce15fd5f2f5e56cce3a300da37cc1df9591e5
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2025-02-11 00:26:16 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        // using extra space ----------------------------
+        // int n = nums.size();
+        // vector<bool> check(n+1,false);
+        // for(int i=0;i<n;i++){
+        //     int ele = nums[i];
+        //     check[ele] = true;
+        // }
+        // for(int i=0;i<=n;i++){
+        //     if(check[i]==false) return i;
+        // }
+        // return 100;
+
+        // without using extra space ---------------------------
         int n = nums.size();
-        vector<bool> check(n+1,false);
+        int i=0;
+        while(i<n){
+            int correctIndex = nums[i];
+            if(i==correctIndex || nums[i]==n) i++;
+            else swap(nums[i],nums[correctIndex]);
+        }
         for(int i=0;i<n;i++){
-            int ele = nums[i];
-            check[ele] = true;
+            if(nums[i]!=i) return i;
         }
-        for(int i=0;i<=n;i++){
-            if(check[i]==false) return i;
-        }
-        return 100;
+        return n;
     }
 };
