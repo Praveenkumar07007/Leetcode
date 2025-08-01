1691cb76618e5c7b4dfe18219524b4f5d87032eb
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2025-08-01 11:18:54 +0530

    Time: 0 ms (100%), Space: 8.8 MB (94.1%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    long long getnCr(int n,int r){
-        long long ans=1;
-        for(int i=0;i<r;i++){
-            ans *= n-i;
-            ans /= i+1;
-        }
-        return ans;
-    }
+
     vector<int> getRow(int n) {
-        vector<int> res;
-        for(int i=0;i<n+1;i++){
-            res.push_back(getnCr(n,i));
+        vector<int> row(n + 1);
+        row[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            row[i] = row[i - 1] * 1LL * (n - i + 1) / i;
         }
-        return res;
+        return row;
     }
+
+
+    // long long getnCr(int n,int r){
+    //     long long ans=1;
+    //     for(int i=0;i<r;i++){
+    //         ans *= n-i;
+    //         ans /= i+1;
+    //     }
+    //     return ans;
+    // }
+    // vector<int> getRow(int n) {
+    //     vector<int> res;
+    //     for(int i=0;i<n+1;i++){
+    //         res.push_back(getnCr(n,i));
+    //     }
+    //     return res;
+    // }
 };
