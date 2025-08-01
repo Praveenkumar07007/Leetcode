e7e4b3bab3e3433d7c331eed21fb58e71fdb0e4c
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2025-08-01 10:36:42 +0530

    Time: 0 ms (100%), Space: 9.7 MB (62.59%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
-        vector<int> prevRow;
-        
-        for (int i = 0; i < numRows; i++) {
-            vector<int> currentRow(i + 1, 1);
-            
-            for (int j = 1; j < i; j++) {
-                currentRow[j] = prevRow[j - 1] + prevRow[j];
+    vector<vector<int>> buildpascal(int n){
+        vector<vector<int>> dp(n); // local DP
+        for(int i = 0; i < n; i++){
+            dp[i].resize(i + 1);       // each row size = i + 1
+            dp[i][0] = dp[i][i] = 1;   // first and last element = 1
+            for(int j = 1; j < i; j++){
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j]; // pascal relation
             }
-            
-            result.push_back(currentRow);
-            prevRow = currentRow;
         }
-        
-        return result;
+        return dp;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        return buildpascal(numRows);
     }
 };
