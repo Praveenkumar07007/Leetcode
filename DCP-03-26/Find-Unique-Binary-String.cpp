226407ca7917546dfe15339f18431480fda53983
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2026-03-08 10:39:09 +0530

    [1980] [Time Beats: 100.00%] [Memory Beats: 35.45%] - LeetPush

--- DCP-03-26/Find-Unique-Binary-String.cpp
+++ DCP-03-26/Find-Unique-Binary-String.cpp
@@ -1,12 +1,28 @@
 1class Solution {
 2public:
 3    string findDifferentBinaryString(vector<string>& nums) {
-4        int n = nums.size();
-5        string ans = "";
-6        for(int i=0;i<n;i++){
-7            char ch = nums[i][i];
-8            ans += (ch=='0')? "1" : "0"; 
-9        }
-10        return ans;
-11    }
-12};
+4        unordered_set<int> st;
+5
+6        for(string &num:nums){
+7            st.insert(stoi(num,0,2));
+8        }
+9        string result = "";
+10        for(int i=0;i<=nums.size();i++){
+11            if(st.find(i)==st.end()){
+12                result = bitset<16>(i).to_string();
+13                break;
+14            }
+15        }
+16
+17        return result.substr(16-nums.size());
+18
+19
+20        // int n = nums.size();
+21        // string ans = "";
+22        // for(int i=0;i<n;i++){
+23        //     char ch = nums[i][i];
+24        //     ans += (ch=='0')? "1" : "0"; 
+25        // }
+26        // return ans;
+27    }
+28};
